a30ff7a8684ba5d9c2a76209e1b63756477d6af6
Unionsilver <121560782+Unionsilver@users.noreply.github.com>
2024-02-28 07:05:06 -0500

    part1 add properties and class

---
+++ src/main/java/com/example/demo/repository/SaleStatisticsRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+
+
+public interface SaleStatisticsRepository extends MongoRepository<SaleStatistics, String> {
+    // Тут можна додати додаткові методи запитів, якщо потрібно
+}
